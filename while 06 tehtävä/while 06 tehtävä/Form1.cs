a75770656a9aec2c5685d6479c8849f324d679aa
Niiranen Milo <niiranenmilo@gmail.com>
2023-10-04 11:13:28 +0300

    Tehtävä 6
    sain tehtävä 6 tehtyä

---
+++ while 06 tehtävä/while 06 tehtävä/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace while_06_tehtävä
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while(true)
+            {
+                DialogResult result = MessageBox.Show("Valitse YES jatkaaksesi tai NO  lopettaaksesi.", "Huom!", MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    continue;
+                }
+                else if (result == DialogResult.No)
+                {
+                    break;
+                }
+            }
+           
+        }
+    }
+}
