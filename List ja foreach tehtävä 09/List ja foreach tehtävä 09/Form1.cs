435f35b57365a86f2318d0dfa4f93bc01710f9da
Niiranen Milo <niiranenmilo@gmail.com>
2023-10-06 13:46:22 +0300

    Tehtävä 09
    korjattu

--- List ja foreach tehtävä 09/List ja foreach tehtävä 09/Form1.cs
+++ List ja foreach tehtävä 09/List ja foreach tehtävä 09/Form1.cs
@@ -12,7 +12,7 @@ namespace List_ja_foreach_tehtävä_09
 {
     public partial class Form1 : Form
     {
-        List<string> lista = new List<string>();
+        List<string> lista;
         public Form1()
         {
             InitializeComponent();
@@ -25,27 +25,38 @@ namespace List_ja_foreach_tehtävä_09
 
         private void button1_Click(object sender, EventArgs e)
         {
-            List<string> lista = new List<string>();
+            lista = new List<string>();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string teksti = textBox1.Text;
 
-            if (!string.IsNullOrWhiteSpace(teksti))
+            if (lista != null)
             {
-                lista.Add(textBox1.Text);
+                string teksti = textBox1.Text;
+                lista.Add(teksti);
                 textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Luo lista ennen kuin lisäät merkkijonoja.");
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            comboBox1.Items.Clear();
-
-            foreach (string merkkijono in lista)
+            if (lista != null)
             {
-                comboBox1.Items.Add(merkkijono);
+                comboBox1.Items.Clear();
+
+                foreach (string merkkijono in lista)
+                {
+                    comboBox1.Items.Add(merkkijono);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Luo lista ennen kuin haet sen sisältöä.");
             }
         }
     }
