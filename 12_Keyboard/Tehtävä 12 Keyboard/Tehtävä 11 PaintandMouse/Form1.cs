96665577e36880dc4146e0fb077913eebef9dfc1
Niiranen Milo <niiranenmilo@gmail.com>
2023-10-10 12:27:14 +0300

    Tehtävä 13
    Tehtävä 13 tehty

---
+++ 12_Keyboard/Tehtävä 12 Keyboard/Tehtävä 11 PaintandMouse/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Tehtävä_11_PaintandMouse
+{
+    public  partial class Form1 : Form
+    {   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Point piste = new Point();
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            
+
+            Graphics Graf = e.Graphics;
+
+            // - Pää 
+
+            Graf.FillEllipse(Brushes.IndianRed, piste.X - 4, piste.Y - 8, 49, 49);
+
+            Graf.DrawEllipse(Pens.Black, piste.X - 4, piste.Y - 8, 49, 49);
+
+            // - Selkä 
+
+            Graf.DrawLine(Pens.Black, piste.X + 21, piste.Y + 41,
+
+            piste.X + 21, piste.Y + 131);
+
+            // - Kädet 
+
+            Graf.DrawLine(Pens.Black, piste.X - 30, piste.Y + 60,
+
+            piste.X + 70, piste.Y + 60);
+
+            // - Jalat 
+
+            Graf.DrawLine(Pens.Black, piste.X + 21, piste.Y + 131,
+            piste.X - 30, piste.Y + 181);
+            Graf.DrawLine(Pens.Black, piste.X + 21, piste.Y + 131,
+
+            piste.X + 70, piste.Y + 181);
+
+
+
+            // Kutsutaan DrawCoordinates()-metodia. 
+
+            DrawCordinates(Graf);
+
+        }
+
+        // DrawCoordinates() -metodi PIIRTÄÄ pisteen koordinaatit lomakkeelle.  
+
+
+
+        private void DrawCordinates(Graphics Graf)
+        {
+
+            // Piirretään piirtokoordinaattien arvot näytölle. 
+
+            Graf.DrawString("(" + piste.X + " ," + piste.Y + ")",
+
+                            new Font("Arial", 14, System.Drawing.FontStyle.Regular),
+
+                            new SolidBrush(Color.Black), 8, 30);
+
+        }
+        
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+
+            {
+                piste = e.Location;
+                Invalidate();
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+
+            {
+                piste = e.Location;
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+
+            {
+                piste = e.Location;
+                Invalidate();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W)
+            {
+                piste.Y -= 1;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+               piste.Y += 1;
+            }
+            else if(e.KeyCode == Keys.D)
+            {
+                piste.X += 1;
+            }
+            else if ((e.KeyCode == Keys.A))
+            {
+                piste.X -= 1;
+            }
+            Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int xKoordinaatt1) && int.TryParse(textBox2.Text, out int yKoordinaatti))
+            {
+                piste = new Point(xKoordinaatt1, yKoordinaatti);
+                Invalidate();
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                piste.Y -= 1;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                piste.Y += 1;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                piste.X += 1;
+            }
+            else if ((e.KeyCode == Keys.A))
+            {
+                piste.X -= 1;
+            }
+            e.SuppressKeyPress = true;
+            Invalidate();
+        }
+    }
+}
---
+++ 13_Picture/Tehtävä 13 Picture/Tehtävä 13 Picture/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tehtävä_13_Picture
+{
+    public partial class Form1 : Form
+    {
+        private Point piste =new Point();
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            kuva = new Bitmap(Properties.Resources.picture04);
+        }
+        Bitmap kuva;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            kuva.MakeTransparent();
+            e.Graphics.DrawImage(kuva, piste);
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                piste = e.Location;
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                piste = e.Location;
+                Invalidate();
+            }
+        }
+    }
+}
