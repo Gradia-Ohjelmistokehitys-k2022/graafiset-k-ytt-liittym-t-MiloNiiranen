6646dd4bf0247a2bb6b0512636b14cd8788cb928
Niiranen Milo <niiranenmilo@gmail.com>
2023-10-11 09:48:11 +0300

    Tehtävä 14
    Tehtävä 14 tehty

--- 14_File/_Projektipohja/Projektipohja/MainForm.Designer.cs
+++ 14_File/_Projektipohja/Projektipohja/MainForm.Designer.cs
@@ -31,47 +31,103 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.MainMenu = new System.Windows.Forms.MenuStrip();
             this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MenuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.MainMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // MainMenu
             // 
             this.MainMenu.BackColor = System.Drawing.Color.Gainsboro;
+            this.MainMenu.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.MainMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.MenuFile});
             this.MainMenu.Location = new System.Drawing.Point(0, 0);
             this.MainMenu.Name = "MainMenu";
-            this.MainMenu.Size = new System.Drawing.Size(284, 24);
+            this.MainMenu.Size = new System.Drawing.Size(610, 36);
             this.MainMenu.TabIndex = 0;
             this.MainMenu.Text = "menuStrip1";
             // 
             // MenuFile
             // 
             this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
             this.MenuFileExit});
+            this.MenuFile.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MenuFile.Name = "MenuFile";
-            this.MenuFile.Size = new System.Drawing.Size(37, 20);
+            this.MenuFile.Size = new System.Drawing.Size(54, 30);
             this.MenuFile.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Image = global::Projektipohja.Properties.Resources.StickmanMenu;
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.newToolStripMenuItem.Text = "New...";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Image = global::Projektipohja.Properties.Resources.StickmanMenu;
+            this.toolStripMenuItem2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(270, 34);
+            this.toolStripMenuItem2.Text = "&Open";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Image = global::Projektipohja.Properties.Resources.StickmanMenu;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Image = global::Projektipohja.Properties.Resources.StickmanMenu;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveAsToolStripMenuItem.Text = "&Save Ass..";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
             // 
             // MenuFileExit
             // 
             this.MenuFileExit.Image = global::Projektipohja.Properties.Resources.StickmanMenu;
             this.MenuFileExit.Name = "MenuFileExit";
-            this.MenuFileExit.Size = new System.Drawing.Size(92, 22);
+            this.MenuFileExit.Size = new System.Drawing.Size(270, 34);
             this.MenuFileExit.Text = "&Exit";
             this.MenuFileExit.Click += new System.EventHandler(this.MenuFileExit_Click);
             // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(583, 361);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(610, 402);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.MainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.MainMenu;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.Text = "Pääformi";
             this.Load += new System.EventHandler(this.MainForm_Load);
@@ -87,6 +143,11 @@
         private System.Windows.Forms.MenuStrip MainMenu;
         private System.Windows.Forms.ToolStripMenuItem MenuFile;
         private System.Windows.Forms.ToolStripMenuItem MenuFileExit;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
     }
 }
 
--- 14_File/_Projektipohja/Projektipohja/MainForm.cs
+++ 14_File/_Projektipohja/Projektipohja/MainForm.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Projektipohja
 {
     public partial class MainForm : Form
     {
+        private String EditorFileName = "Untitled";
         public MainForm()
         {
             InitializeComponent();
+            richTextBox1.Dock = DockStyle.Fill;
         }
 
         private void MenuFileExit_Click(object sender, EventArgs e)
@@ -25,6 +28,123 @@ namespace Projektipohja
         private void MainForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDlg = new OpenFileDialog();
+
+            OpenFileDlg.Title = "Open";
+
+            OpenFileDlg.ShowReadOnly = true;
+
+            OpenFileDlg.Filter = "Text documents (*.txt)|*.txt|All files|*.*";
+            // Avataan windowsin standardi avausdialogi. 
+
+            if (OpenFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                // Talletetaan tiedoston nimi ja polku lukemista varten. 
+
+                EditorFileName = OpenFileDlg.FileName;
+
+                // Luetaan tiedosotn sisältö ja tuodaan se näytölle. 
+
+                ReadFile();
+                SetFormTitleText();
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Luodaan standardi talletusdialogiobjekti ja alustetaan se. 
+
+            SaveFileDialog SaveFileDlg = new SaveFileDialog();
+
+            SaveFileDlg.Filter = "Text documents (*.txt)|*.txt|All files|*.*";
+
+
+            // Avataan windowsin standardi talletusdialogi. 
+
+            if (SaveFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                // Talletetaan tiedoston nimi ja polku talletusta varten. 
+
+                EditorFileName = SaveFileDlg.FileName;
+
+                SaveFile();
+                SetFormTitleText();
+
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EditorFileName = "Untitled";
+            richTextBox1.Clear();
+            SetFormTitleText();
+        }
+        private void SetFormTitleText()
+        {   
+            FileInfo fileinfo = new FileInfo(EditorFileName);
+
+            Text = fileinfo.Name + " - Editor";
+
+        }
+        private void ReadFile()
+        {
+            try
+            {
+
+                // Luodaan StreamReader objekti ja luetaan tiedosto. 
+
+                using (StreamReader Reader = new StreamReader(EditorFileName))
+                {
+
+                    // Luku 
+
+                    richTextBox1.Clear();
+
+                    richTextBox1.Text = Reader.ReadToEnd();
+
+                }
+
+            }
+
+            catch (IOException ex)
+            {
+
+                MessageBox.Show("Error: " + ex.Message, "Open File",
+
+                                 MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            }
+        }
+
+        private void SaveFile()
+        {
+            try
+            {
+                using (StreamWriter StrWriter = new StreamWriter(EditorFileName))
+                {
+                    StrWriter.Write(richTextBox1.Text);
+                }
+            }
+
+            catch (IOException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Save File",
+                                 MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                SaveFile();
         }
     }
 }
